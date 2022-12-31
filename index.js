da593b81c947b1c20b94df94eb136c9f6c1a0239
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-31 18:53:48 +0500

    Upadting the Design

---
+++ index.js
@@ -0,0 +1,6 @@
+let typed=new Typed(".auto-text",{
+    strings: ["FrontEnd Web Developer..","FrontEnd Web Designer..","BackEnd Web Developer.."],
+    typeSpeed:200,
+    backSpeed:100,
+    loop: true
+    });
