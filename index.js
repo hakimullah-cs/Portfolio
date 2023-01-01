bb08ac9ade3ac8c3d0e762a6c65433e2f2589ef2
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-01-01 10:34:48 +0500

    Adding show class on navbar links and set the setTimeout

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+let navbar=document.querySelector('.collapse').querySelectorAll('a');
+console.log(navbar);
+navbar.forEach((ele)=>{
+    ele.addEventListener('click',function(){
+        navbar.forEach((nav)=>{nav.classList.remove('show')});
+        this.classList.add('show');
+    });
+});
+setTimeout(function(){
+    alert("Hello Sir/Madam! Welcome to my personal Website & You have any query plx contact me!");
+},7000);
+
+
+
+
+
 let typed=new Typed(".auto-text",{
-    strings: ["FrontEnd Web Developer..","FrontEnd Web Designer..","BackEnd Web Developer.."],
+    strings: ["FrontEnd Web Developer..!","FrontEnd Web Designer..!","UI/UX Designer..!"],
     typeSpeed:200,
     backSpeed:100,
     loop: true
