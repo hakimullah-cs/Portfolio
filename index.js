f7296877b8f6189da3b0dd196e552db4d538d5b3
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-01-03 21:01:32 +0500

    Adding Filterable Project Section

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
-let navbar=document.querySelector('.collapse').querySelectorAll('a');
-console.log(navbar);
-navbar.forEach((ele)=>{
-    ele.addEventListener('click',function(){
-        navbar.forEach((nav)=>{nav.classList.remove('show')});
-        this.classList.add('show');
+let navbar = document.querySelector(".collapse").querySelectorAll("a");
+navbar.forEach((ele) => {
+  ele.addEventListener("click", function () {
+    navbar.forEach((nav) => {
+      nav.classList.remove("show");
     });
+    this.classList.add("show");
+  });
 });
-setTimeout(function(){
-    alert("Hello Sir/Madam! Welcome to my personal Website & You have any query plx contact me!");
-},7000);
 
 
 
+let typed = new Typed(".auto-text", {
+  strings: [
+    "FrontEnd Web Developer..!",
+    "FrontEnd Web Designer..!",
+    "UI/UX Designer..!",
+  ],
+  typeSpeed: 200,
+  backSpeed: 100,
+  loop: true,
+});
+
+// ============== Popup Code================
+// window.addEventListener('scroll',(e)=>{
+//   e.preventDefault();
+  
+//   if(window.scrollY>900){
+//    let timeout= setTimeout(function () {
+//       var popup = document.querySelector(".popup");
+//       popup.style.display = "block";
+//       let dismiss = document
+//         .querySelector(".dismiss")
+//         .addEventListener("click", () => {
+//           popup.style.display = "none";
+//         });
+//     }, 7000);
+//   }
+//   if(window.scrollY<=0){
+//     clearTimeout(timeout);
+//   }
+ 
+// });
 
 
-let typed=new Typed(".auto-text",{
-    strings: ["FrontEnd Web Developer..!","FrontEnd Web Designer..!","UI/UX Designer..!"],
-    typeSpeed:200,
-    backSpeed:100,
-    loop: true
-    });
+// ============== Projects Section JS Code===================
+let filterbtn=document.querySelectorAll('.gallery .Filter');
+console.log(filterbtn);
+let project=document.querySelectorAll('.projects .img-overlay');
+console.log(project);
+
+filterbtn.forEach((item)=>{
+   item.addEventListener('click',()=>{
+      filterbtn.forEach((btn)=>{
+        btn.classList.remove('active');
+      });
+      item.classList.add('active');
+      project.forEach((show)=>{
+         show.style.display="none";
+         let proj=item.textContent.toLowerCase();
+         if(show.getAttribute('data-item')===proj || proj==='all'){
+          show.style.display="block";
+         }
+      });
+   });
+});
